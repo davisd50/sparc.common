bc0e880b39a7077bb5a5ff29f1a49aa135b51502
David Davis <davisd50@gmail.com>
2016-01-16 08:59:14 -0500

    move zcml configuration items into sparc.configuration

--- sparc/common/__init__.py
+++ sparc/common/__init__.py
@@ -1,6 +1,4 @@
 from interfaces import ICallable
-from configure import Configure
-from configure import ConfigurationRequired
 
 from zope.i18nmessageid import MessageFactory
 SPARCMessageFactory = MessageFactory('sparc.common')

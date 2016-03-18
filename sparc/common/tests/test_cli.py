34a43a40b37d4265aeba44a6835e2777aa5c4c6d
David Davis <davisd50@gmail.com>
2016-03-18 09:05:45 -0400

    update for test layer

--- sparc/common/tests/test_cli.py
+++ sparc/common/tests/test_cli.py
@@ -1,15 +1,11 @@
-"""Test
-"""
 import unittest
-from doctest import DocTestSuite
-from doctest import DocFileSuite
+from sparc.testing.fixture import test_suite_mixin
 
-import sparc.common
 
-def test_suite():
-    return unittest.TestSuite((
-        DocFileSuite('cli.txt',
-                     package=sparc.common),))
+class test_suite(test_suite_mixin):
+    package = 'sparc.common'
+    module = 'cli'
+
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
